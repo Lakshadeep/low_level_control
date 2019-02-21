95793321a5f10f9a50419fd903f8f38918edaab7
Lakshadeep Naik <lakshadeep.naik@gmail.com>
2019-02-21 20:13:49 +0100

    Added desired velocity subscriber

--- heading_control/include/heading_control/heading_control_ros.h
+++ heading_control/include/heading_control/heading_control_ros.h
@@ -24,23 +24,24 @@ public:
 private:
   // ROS
   ros::NodeHandle nh_;
-  ros::Subscriber desired_heading_subscriber_, monitored_heading_subscriber_ ;
+  ros::Subscriber desired_heading_subscriber_, desired_velocity_subscriber_, monitored_heading_subscriber_ ;
   ros::ServiceServer heading_control_switch_service_;
   ros::Publisher operator_cmd_publisher_;
 
   // Parameters
-  std::string desired_heading_topic_, monitored_heading_topic_ ;
+  std::string desired_heading_topic_, desired_velocity_topic_, monitored_heading_topic_ ;
 
   // robot heading monitoring
   HeadingControl heading_control_;
   bool is_enabled_; 
-  double monitored_heading_, desired_heading_;
+  double monitored_heading_, desired_heading_, desired_velocity_;
   double heading_control_velocity_;
 
 
   // Members
   void loadParameters();
   void desiredHeadingCallback(const std_msgs::Float32ConstPtr& desired_heading_msg);
+  void desiredVelocityCallback(const std_msgs::Float32ConstPtr& desired_velocity_msg);
   void monitoredHeadingCallback(const std_msgs::Float32ConstPtr& monitored_heading_msg);
   bool headingControlSwitch(heading_control::Switch::Request  &req, heading_control::Switch::Response &res);
 
--- heading_control/src/heading_control_ros.cpp
+++ heading_control/src/heading_control_ros.cpp
@@ -1,9 +1,11 @@
 #include "heading_control/heading_control_ros.h"
 
-HeadingControlROS::HeadingControlROS(ros::NodeHandle& nh): nh_(nh), monitored_heading_(0), desired_heading_(0), is_enabled_(false)
+HeadingControlROS::HeadingControlROS(ros::NodeHandle& nh): nh_(nh), monitored_heading_(0), desired_heading_(0), is_enabled_(false),
+desired_velocity_(0)
 {
     loadParameters();
     desired_heading_subscriber_ = nh_.subscribe(desired_heading_topic_, 1, &HeadingControlROS::desiredHeadingCallback, this);
+    desired_velocity_subscriber_ = nh_.subscribe(desired_velocity_topic_, 1, &HeadingControlROS::desiredVelocityCallback, this);
     monitored_heading_subscriber_ = nh_.subscribe(monitored_heading_topic_, 1, &HeadingControlROS::monitoredHeadingCallback, this);
     heading_control_switch_service_ = nh.advertiseService("/heading_control_switch", &HeadingControlROS::headingControlSwitch, this);
     operator_cmd_publisher_ = nh_.advertise<nav2d_operator::cmd>("/cmd", 1);
@@ -16,6 +18,11 @@ HeadingControlROS::~HeadingControlROS()
 void HeadingControlROS::desiredHeadingCallback(const std_msgs::Float32ConstPtr& desired_heading_msg)
 {
     desired_heading_ = desired_heading_msg->data;
+}
+
+void HeadingControlROS::desiredVelocityCallback(const std_msgs::Float32ConstPtr& desired_velocity_msg)
+{
+    desired_velocity_ = desired_velocity_msg->data;
 }
 
 void HeadingControlROS::monitoredHeadingCallback(const std_msgs::Float32ConstPtr& monitored_heading_msg)
@@ -31,7 +38,7 @@ void HeadingControlROS::run()
     if(is_enabled_)
     {
         turn = heading_control_.computeDirection(monitored_heading_, desired_heading_);
-        velocity = heading_control_velocity_;
+        velocity = desired_velocity_;
     }
 
     nav2d_operator::cmd cmd_msg;
@@ -44,18 +51,18 @@ void HeadingControlROS::run()
 
 void HeadingControlROS::loadParameters()
 {
-    std::string desired_heading_topic, monitored_heading_topic;
+    std::string desired_heading_topic, desired_velocity_topic, monitored_heading_topic;
     double heading_control_velocity;
 
     nh_.param<std::string>("desired_heading_topic", desired_heading_topic, "/desired_heading");
-    nh_.param<std::string>("monitored_headin_topic", monitored_heading_topic, "/monitored_heading");
-    nh_.param("heading_control_velocity", heading_control_velocity, 0.1);
+    nh_.param<std::string>("monitored_heading_topic", monitored_heading_topic, "/monitored_heading");
+    nh_.param<std::string>("desired_velocity_topic", desired_velocity_topic, "/desired_velocity");
     desired_heading_topic_ = desired_heading_topic;
+    desired_velocity_topic_ = desired_velocity_topic;
     monitored_heading_topic_ = monitored_heading_topic;
-    heading_control_velocity_ = heading_control_velocity;
     ROS_DEBUG("desired_heading_topic: %s", desired_heading_topic_.c_str());
     ROS_DEBUG("monitored_heading_topic: %s", monitored_heading_topic_.c_str());
-    ROS_DEBUG("heading_control_velocity: %f", heading_control_velocity_);
+    ROS_DEBUG("desired_velocity_topic: %s", desired_heading_topic_.c_str());
 }
 
 bool HeadingControlROS::headingControlSwitch(heading_control::Switch::Request  &req, heading_control::Switch::Response &res)
