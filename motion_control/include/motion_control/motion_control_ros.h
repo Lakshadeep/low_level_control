61b7f66ef4673c4a93e062ce0471aab733fd6b98
Lakshadeep Naik <lakshadeep.naik@gmail.com>
2019-02-28 16:24:38 +0100

    Moved methods from nav_2d operator

--- motion_control/include/motion_control/motion_control_ros.h
+++ motion_control/include/motion_control/motion_control_ros.h
@@ -1,6 +1,8 @@
 #ifndef MOTION_CONTROL_ROS_H
 #define MOTION_CONTROL_ROS_H
 
+#define LUT_RESOLUTION 100
+
 #include <string>
 #include <cmath>
 
@@ -25,6 +27,11 @@ public:
     MotionControlROS(ros::NodeHandle&);
     ~MotionControlROS();
 
+    /*
+    @brief Generates and sends Twist-Message to Robot
+    This is the Operator's core function and should be called periodically
+     */
+    void executeCommand();
 
 private:
     // ROS sub/pub
@@ -37,17 +44,21 @@ private:
     ros::Publisher cost_publisher_;
 
     // ROS tf
-    tf::TransformListener tf_listener;
+    tf::TransformListener tf_listener_;
     tf2_ros::Buffer tf2_buffer;
     tf2_ros::TransformListener tf2_listener;
 
     // costmap params
     costmap_2d::Costmap2DROS* local_map;
     costmap_2d::Costmap2D* costmap;
-    double raster_size;
+    double raster_size_;
 
     // Parameters
-    std::string motion_command_topic_, velocity_command_topic_, robot_frame_, odometry_frame_;;
+    std::string motion_command_topic_, velocity_command_topic_, robot_frame_, odometry_frame_;
+
+    double max_free_space_, safety_decay_, safety_weight_, conformance_weight_, continue_weight_, escape_weight_, 
+           max_velocity_, desired_safety_threshold_;
+    unsigned int recovery_steps_;
 
     // robot motion monitoring
     MotionControl motion_control_;
@@ -62,10 +73,52 @@ private:
     double current_velocity_;
     double current_direction_;
 
+    // other internal vars
+    sensor_msgs::PointCloud* trajectory_table[(LUT_RESOLUTION * 4) + 2];
+
     // Members
     void loadParameters();
     void motionCommandCallback(const motion_control::CommandConstPtr& motion_cmd_msg);
     bool motionControlSwitch(motion_control::Switch::Request  &req, motion_control::Switch::Response &res);
+
+    /*
+    @brief Initializes look-up tables
+    This calculates the trajectories of all possible direction commands.
+    Must be called once before the Operator is used
+    */
+    void initTrajTable();
+
+    /*
+    @brief Calculates the distance the robot can move following the given trajectory
+    @param cloud PointCloud defining a trajectory
+    @return Number of free cells
+    */
+    int calculateFreeSpace(sensor_msgs::PointCloud* cloud);
+    
+
+    /*
+    @brief Calculate the action value of a given command
+    @param direction How to move the robot
+    @param velocity Only used to distinguish forward and backward motion
+    @param debug Publish result of evaluation functions on debug topic
+    @return Weighted sum of all evaluation functions
+    The given action is rated by 4 different evaluation functions:
+    Free Space: How far can the robot move following this command
+    Safety: How close will it get near obstacles
+    Conformance: How good does it follow the commanded direction
+    Continuity: (experimental!) How does it conform with the last issued command
+     */
+    double evaluateAction(double direction, double velocity, bool debug);
+
+    /**
+    @brief Get the trajectory defined by the given movement command
+    @param direction How to move the robot
+    @param velocity Only used to distinguish forward and backward motion
+    @return A pointer to the PointCloud defined in the robot coordinate frame
+     */
+    sensor_msgs::PointCloud* getPointCloud(double direction, double velocity);
+    double findBestDirection();
+    double diff(double v1, double v2);
 
 };
 
--- motion_control/src/motion_control_ros.cpp
+++ motion_control/src/motion_control_ros.cpp
@@ -1,7 +1,7 @@
 #include "motion_control/motion_control_ros.h"
 
 MotionControlROS::MotionControlROS(ros::NodeHandle& nh): nh_(nh), is_enabled_(false), tf2_buffer(), tf2_listener(tf2_buffer), 
-tf_listener(ros::Duration(0.5))
+tf_listener_(ros::Duration(0.5))
 {
     loadParameters();
 
@@ -11,12 +11,12 @@ tf_listener(ros::Duration(0.5))
     velocity_command_publisher_ = nh_.advertise<motion_control::Command>(velocity_command_topic_, 1);
 
     // Create the local costmap
-    local_map = new costmap_2d::Costmap2DROS(std::string("local_map"), tf_listener);
-    raster_size = local_map->getCostmap()->getResolution();
+    local_map = new costmap_2d::Costmap2DROS(std::string("local_map"), tf_listener_);
+    raster_size_ = local_map->getCostmap()->getResolution();
 
     // Apply tf_prefix to all used frame-id's
-    robot_frame_ = tf_listener.resolve(robot_frame_);
-    odometry_frame_ = tf_listener.resolve(odometry_frame_);
+    robot_frame_ = tf_listener_.resolve(robot_frame_);
+    odometry_frame_ = tf_listener_.resolve(odometry_frame_);
 }
 
 MotionControlROS::~MotionControlROS()
@@ -62,42 +62,42 @@ void MotionControlROS::loadParameters()
     
     double max_free_space;
     nh_.param<double>("max_free_space", max_free_space, 5.0);
-    // TODO
+    max_free_space_ = max_free_space;
     ROS_DEBUG("max_free_space: %f", max_free_space);
 
     double safety_decay;
     nh_.param<double>("safety_decay", safety_decay, 0.95);
-    // TODO
+    safety_decay_ = safety_decay;
     ROS_DEBUG("safety_decay: %f", safety_decay);
 
     double safety_weight;
     nh_.param<double>("safety_weight", safety_weight, 0.95);
-    // TODO
+    safety_weight_ = safety_weight;
     ROS_DEBUG("safety_weight: %f", safety_weight);
 
     double conformance_weight;
     nh_.param<double>("conformance_weight", conformance_weight, 1);
-    // TODO
+    conformance_weight_ = conformance_weight;
     ROS_DEBUG("conformance_weight: %f", conformance_weight);
 
     double continue_weight;
     nh_.param<double>("continue_weight", continue_weight, 1);
-    // TODO
+    continue_weight_ = continue_weight;
     ROS_DEBUG("continue_weight: %f", continue_weight);
 
     double escape_weight;
     nh_.param<double>("escape_weight", escape_weight, 1);
-    // TODO
+    escape_weight_ = escape_weight;
     ROS_DEBUG("escape_weight: %f", escape_weight);
 
     double max_velocity;
     nh_.param<double>("max_velocity", max_velocity, 1);
-    // TODO
+    max_velocity_ = max_velocity;
     ROS_DEBUG("max_velocity: %f", max_velocity);
 
     double desired_safety_threshold;
-    nh_.param<double>("desired_safety_threshold", desired_safety_threshold, 1);
-    // TODO
+    nh_.param<double>("desired_safety_threshold", desired_safety_threshold, 0.5);
+    desired_safety_threshold_ = desired_safety_threshold;
     ROS_DEBUG("desired_safety_threshold: %f", desired_safety_threshold);
 }
 
@@ -106,4 +106,445 @@ bool MotionControlROS::motionControlSwitch(motion_control::Switch::Request  &req
     is_enabled_ = req.enable;
     res.status = true;
     return true;
+}
+
+
+void MotionControlROS::initTrajTable()
+{
+    for (int i = 0; i < (LUT_RESOLUTION * 4) + 2; i++)
+    {
+        trajectory_table[i] = NULL;
+    }
+    for (int i = 1; i < LUT_RESOLUTION; i++)
+    {
+        double tw = -M_PI * i / LUT_RESOLUTION;
+        double tx = cos(tw) + 1;
+        double ty = -sin(tw);
+        double tr = ((tx * tx) + (ty * ty)) / (ty + ty);
+        std::vector<geometry_msgs::Point32> points;
+        double alpha = 0;
+        while (alpha < M_PI)
+        {
+            double x = tr * sin(alpha);
+            double y = tr * (1.0 - cos(alpha));
+            geometry_msgs::Point32 p;
+            p.x = x;
+            p.y = y;
+            p.z = 0;
+            points.push_back(p);
+            alpha += raster_size_ / tr;
+        }
+        // Add the PointCloud to the LUT
+        // Circle in forward-left direction
+        sensor_msgs::PointCloud* flcloud = new sensor_msgs::PointCloud();
+        flcloud->header.stamp = ros::Time(0);
+        flcloud->header.frame_id = robot_frame_;
+        flcloud->points.resize(points.size());
+
+        // Circle in forward-right direction
+        sensor_msgs::PointCloud* frcloud = new sensor_msgs::PointCloud();
+        frcloud->header.stamp = ros::Time(0);
+        frcloud->header.frame_id = robot_frame_;
+        frcloud->points.resize(points.size());
+
+        // Circle in backward-left direction
+        sensor_msgs::PointCloud* blcloud = new sensor_msgs::PointCloud();
+        blcloud->header.stamp = ros::Time(0);
+        blcloud->header.frame_id = robot_frame_;
+        blcloud->points.resize(points.size());
+
+        // Circle in backward-right direction
+        sensor_msgs::PointCloud* brcloud = new sensor_msgs::PointCloud();
+        brcloud->header.stamp = ros::Time(0);
+        brcloud->header.frame_id = robot_frame_;
+        brcloud->points.resize(points.size());
+
+        for (unsigned int j = 0; j < points.size(); j++)
+        {
+            flcloud->points[j] = points[j];
+            frcloud->points[j] = points[j];
+            blcloud->points[j] = points[j];
+            brcloud->points[j] = points[j];
+
+            frcloud->points[j].y *= -1;
+            blcloud->points[j].x *= -1;
+            brcloud->points[j].x *= -1;
+            brcloud->points[j].y *= -1;
+        }
+        trajectory_table[LUT_RESOLUTION - i] = flcloud;
+        trajectory_table[LUT_RESOLUTION + i] = frcloud;
+        trajectory_table[(3 * LUT_RESOLUTION + 1) - i] = blcloud;
+        trajectory_table[(3 * LUT_RESOLUTION + 1) + i] = brcloud;
+    }
+
+    // Add First and Last LUT-element
+    geometry_msgs::Point32 p;
+    p.x = 0;
+    p.y = 0;
+    p.z = 0;
+
+    sensor_msgs::PointCloud* turn = new sensor_msgs::PointCloud();
+    turn->header.stamp = ros::Time(0);
+    turn->header.frame_id = robot_frame_;
+    turn->points.resize(1);
+    turn->points[0] = p;
+
+    int straight_len = 5.0 / raster_size_;
+
+    sensor_msgs::PointCloud* fscloud = new sensor_msgs::PointCloud();
+    fscloud->header.stamp = ros::Time(0);
+    fscloud->header.frame_id = robot_frame_;
+    fscloud->points.resize(straight_len);
+
+    sensor_msgs::PointCloud* bscloud = new sensor_msgs::PointCloud();
+    bscloud->header.stamp = ros::Time(0);
+    bscloud->header.frame_id = robot_frame_;
+    bscloud->points.resize(straight_len);
+
+    for (int i = 0; i < straight_len; i++)
+    {
+        fscloud->points[i] = p;
+        bscloud->points[i] = p;
+        bscloud->points[i].x *= -1;
+        p.x += raster_size_;
+    }
+
+    trajectory_table[LUT_RESOLUTION] = fscloud;
+    trajectory_table[LUT_RESOLUTION * 3 + 1] = bscloud;
+
+    trajectory_table[0] = turn;
+    trajectory_table[LUT_RESOLUTION * 2] = turn;
+    trajectory_table[LUT_RESOLUTION * 2 + 1] = turn;
+    trajectory_table[LUT_RESOLUTION * 4 + 1] = turn;
+
+    for (int i = 0; i < (LUT_RESOLUTION * 4) + 2; i++)
+    {
+        if (!trajectory_table[i])
+        {
+            ROS_ERROR("Table entry %d has not been initialized!", i);
+        }
+    }
+}
+
+
+int MotionControlROS::calculateFreeSpace(sensor_msgs::PointCloud* cloud)
+{
+    unsigned int mx, my;
+    int length = cloud->points.size();
+    int freeSpace = 0;
+    for (int i = 0; i < length; i++)
+    {
+        if (costmap->worldToMap(cloud->points[i].x, cloud->points[i].y, mx, my))
+        {
+            if (costmap->getCost(mx, my) < costmap_2d::INSCRIBED_INFLATED_OBSTACLE)
+            {
+                freeSpace++;
+            } else
+            {
+                break;
+            }
+        } else
+        {
+            break;
+        }
+    }
+    return freeSpace;
+}
+
+sensor_msgs::PointCloud* MotionControlROS::getPointCloud(double direction, double velocity)
+{
+    if (direction < -1) direction = -1;
+    if (direction > 1) direction = 1;
+    int offset = (velocity >= 0) ? LUT_RESOLUTION : 3 * LUT_RESOLUTION + 1;
+    int table_index = (direction * LUT_RESOLUTION) + offset;
+    return trajectory_table[table_index];
+}
+
+
+double MotionControlROS::evaluateAction(double direction, double velocity, bool debug)
+{
+    sensor_msgs::PointCloud* originalCloud = getPointCloud(direction, velocity);
+    sensor_msgs::PointCloud transformedCloud;
+    try
+    {
+        tf_listener_.transformPointCloud(odometry_frame_, *originalCloud, transformedCloud);
+    }
+    catch (tf::TransformException ex)
+    {
+        ROS_ERROR("%s", ex.what());
+        return 1;
+    }
+
+    double value_safety = 0.0;      // How safe is it to move in that direction?
+    double value_escape = 0.0;      // How much does the safety improve?
+    double value_conformance = 0.0; // How conform is it with the desired direction?
+    double value_continue = 0.0;    // How conform is it with the previous command?
+
+    double decay = 1.0;
+    double safe_max = 0.0;
+    double cost_max = 0.0;
+    double cost_start = 1.0;
+
+    // Calculate safety and escape value
+    int length = transformedCloud.points.size();
+    for (int i = 0; i < length; i++)
+    {
+        unsigned int mx, my;
+        double cell_cost;
+        if (costmap->worldToMap(transformedCloud.points[i].x, transformedCloud.points[i].y, mx, my))
+        {
+            cell_cost = (double)costmap->getCost(mx, my) / costmap_2d::INSCRIBED_INFLATED_OBSTACLE;
+            if (cell_cost >= 1.0)
+            {
+                // Trajectory hit an obstacle
+                break;
+            }
+        }
+        if (i == 0)
+            cost_start = cell_cost;
+        double cost = cell_cost * decay;
+        double safe = (cost_start - cell_cost) * decay * 2.0;
+
+        if (cost > cost_max) cost_max = cost;
+        if (safe > safe_max) safe_max = safe;
+
+        decay *= safety_decay_;
+    }
+
+    double action_value = 0.0;
+    double norm_factor = 0.0;
+
+    // Add safety value
+    value_safety = 1.0 - cost_max;
+    action_value += value_safety * safety_weight_;
+    norm_factor += safety_weight_;
+
+    // Add escape value
+    value_escape = safe_max;
+    action_value += value_escape * escape_weight_;
+    norm_factor += escape_weight_;
+
+    if (recovery_steps_ == 0)
+    {
+        // Calculate continuety value
+        value_continue = (current_direction_ - direction) * 0.5;
+        value_continue = 1.0 - (value_continue * value_continue);
+
+        // Calculate conformance value
+        double corr = (desired_direction_ - direction) * M_PI;
+        value_conformance = 0.5 * cos(corr) + 0.5;
+
+        // Add both to action value
+        action_value += value_conformance * conformance_weight_;
+        action_value += value_continue * continue_weight_;
+        norm_factor += conformance_weight_ + continue_weight_;
+    }
+
+    action_value /= norm_factor;
+
+    if (debug)
+    {
+        geometry_msgs::Vector3 cost_msg;
+        cost_msg.x = value_safety;
+        cost_msg.y = value_escape;
+        cost_msg.z = value_conformance;
+        //mCostPublisher.publish(cost_msg);
+    }
+
+    return action_value;
+}
+
+double MotionControlROS::findBestDirection()
+{
+    double best_dir = -1.0;
+    double best_value = 0.0;
+    double step = 0.01;
+    double dir = -1.0;
+
+    while (dir <= 1.0)
+    {
+        double value = evaluateAction(dir, desired_velocity_, false);
+        if (value > best_value)
+        {
+            best_dir = dir;
+            best_value = value;
+        }
+        dir += step;
+    }
+    return best_dir;
+}
+
+double MotionControlROS::diff(double v1, double v2)
+{
+    if (v1 > v2)
+        return v1 - v2;
+    else
+        return v2 - v1;
+}
+
+
+void MotionControlROS::executeCommand()
+{
+    // 1. Get a copy of the costmap to work on.
+    costmap = local_map->getCostmap();
+    boost::unique_lock<costmap_2d::Costmap2D::mutex_t> lock(*(costmap->getMutex()));
+    double best_direction, d, desired_direction_val;
+
+    // 2. Set velocity and direction depending on drive mode
+    switch (drive_mode_)
+    {
+    case 0:
+        best_direction = findBestDirection();
+        desired_direction_val = evaluateAction(desired_direction_, current_velocity_, false);
+        if (desired_direction_val >= desired_safety_threshold_) best_direction = desired_direction_;
+        d = best_direction - current_direction_;
+        // if (d < -0.2) d = -0.2;
+        // if (d > 0.2) d = 0.2;
+        current_direction_ += d;
+        current_velocity_ = desired_velocity_;
+        break;
+    case 1:
+        current_direction_ = desired_direction_;
+        current_velocity_ = desired_velocity_;
+        break;
+    default:
+        ROS_ERROR("Invalid drive mode!");
+        current_velocity_ = 0.0;
+    }
+
+    // Create some Debug-Info
+    evaluateAction(current_direction_, current_velocity_, true);
+
+    sensor_msgs::PointCloud* originalCloud = getPointCloud(current_direction_, desired_velocity_);
+    sensor_msgs::PointCloud transformedCloud;
+
+    try
+    {
+        tf_listener_.transformPointCloud(odometry_frame_, *originalCloud, transformedCloud);
+    }
+    catch (tf::TransformException ex)
+    {
+        ROS_ERROR("%s", ex.what());
+        return;
+    }
+
+    // Determine maximum linear velocity
+    int free_cells = calculateFreeSpace(&transformedCloud);
+    double free_space = raster_size_ * free_cells;
+
+    double safe_velocity_ = (free_space / max_free_space_) + 0.05;
+    if (free_cells == transformedCloud.points.size() && safe_velocity_ < 0.5)
+        safe_velocity_ = 0.5;
+
+    if (free_space < 0.3 && free_cells < transformedCloud.points.size())
+        safe_velocity_ = 0;
+
+    if (safe_velocity_ > max_velocity_)
+        safe_velocity_ = max_velocity_;
+
+    // Check whether the robot is stuck
+    if (recovery_steps_ > 0) recovery_steps_--;
+    if (safe_velocity_ < 0.1)
+    {
+        if (drive_mode_ == 0)
+        {
+            recovery_steps_ = 30; // Recover for 3 seconds
+            ROS_WARN_THROTTLE(1, "Robot is stuck! Trying to recover...");
+        } else
+        {
+            current_velocity_ = 0;
+            ROS_WARN_THROTTLE(1, "Robot cannot move further in this direction!");
+        }
+    }
+
+    // Publish route via ROS (mainly for debugging)
+    if (false)
+    {
+        nav_msgs::GridCells route_msg;
+        route_msg.header.frame_id = odometry_frame_;
+        route_msg.header.stamp = ros::Time::now();
+
+        route_msg.cell_width = costmap->getResolution();
+        route_msg.cell_height = costmap->getResolution();
+
+        route_msg.cells.resize(free_cells);
+        for (int i = 0; i < free_cells; i++)
+        {
+            route_msg.cells[i].x = transformedCloud.points[i].x;
+            route_msg.cells[i].y = transformedCloud.points[i].y;
+            route_msg.cells[i].z = transformedCloud.points[i].z;
+        }
+        //mTrajectoryPublisher.publish(route_msg);
+
+        // Publish plan via ROS (mainly for debugging)
+        sensor_msgs::PointCloud* originalPlanCloud = getPointCloud(desired_direction_, desired_velocity_);
+        sensor_msgs::PointCloud transformedPlanCloud;
+
+        try
+        {
+            tf_listener_.transformPointCloud(odometry_frame_, *originalPlanCloud, transformedPlanCloud);
+        }
+        catch (tf::TransformException ex)
+        {
+            ROS_ERROR("%s", ex.what());
+            return;
+        }
+        nav_msgs::GridCells plan_msg;
+        plan_msg.header = route_msg.header;
+
+        plan_msg.cell_width = costmap->getResolution();
+        plan_msg.cell_height = costmap->getResolution();
+
+        int free_space_plan = calculateFreeSpace(&transformedPlanCloud);
+        plan_msg.cells.resize(free_space_plan);
+        for (int i = 0; i < free_space_plan; i++)
+        {
+            plan_msg.cells[i].x = transformedPlanCloud.points[i].x;
+            plan_msg.cells[i].y = transformedPlanCloud.points[i].y;
+            plan_msg.cells[i].z = transformedPlanCloud.points[i].z;
+        }
+        //mPlanPublisher.publish(plan_msg);
+    }
+
+    // Publish result via Twist-Message
+    geometry_msgs::Twist velocity_command_msg_;
+    double velocity = current_velocity_;
+    if (current_direction_ == 0)
+    {
+        if (velocity > safe_velocity_)
+        {
+            ROS_DEBUG("Desired velocity of %.2f is limited to %.2f", velocity, safe_velocity_);
+            velocity = safe_velocity_;
+        } else if (velocity < -safe_velocity_)
+        {
+            ROS_DEBUG("Desired velocity of %.2f is limited to %.2f", velocity, -safe_velocity_);
+            velocity = -safe_velocity_;
+        }
+        velocity_command_msg_.linear.x = velocity;
+        velocity_command_msg_.angular.z = 0;
+    } else if (current_direction_ == -1 || current_direction_ == 1)
+    {
+        velocity_command_msg_.linear.x = 0;
+        velocity_command_msg_.angular.z = -1.0 * current_direction_ * velocity;
+    } else
+    {
+        double x = sin(current_direction_ * M_PI);
+        double y = (cos(current_direction_ * M_PI) + 1);
+        double r = ((x * x) + (y * y)) / (2 * x);
+        double abs_r = (r > 0) ? r : -r;
+        velocity /= (1 + (1.0 / abs_r));
+        if (velocity > safe_velocity_)
+        {
+            ROS_DEBUG("Desired velocity of %.2f is limited to %.2f", velocity, safe_velocity_);
+            velocity = safe_velocity_;
+        } else if (velocity < -safe_velocity_)
+        {
+            ROS_DEBUG("Desired velocity of %.2f is limited to %.2f", velocity, -safe_velocity_);
+            velocity = -safe_velocity_;
+        }
+
+        velocity_command_msg_.linear.x = velocity;
+        velocity_command_msg_.angular.z = -1.0 / r * velocity_command_msg_.linear.x;
+    }
+    velocity_command_publisher_.publish(velocity_command_msg_);
 }
